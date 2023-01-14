930a1c7fa80972b520f2cab530342ac2c0ca4a7a
jeff4lvi <jeff_alvi@hotmail.com>
2023-01-14 20:38:08 -0300

    primeiro  first commit

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/summary";
+
+export function Transactions() {
+    return (
+    <div>
+       <Header />
+       <Summary />
+
+       
+    </div>
+    )
+}
